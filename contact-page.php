e019253abf3373c2352b4b5b5c3e23a0f4fcb749
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2018-11-23 12:44:01 +0100

    Order

--- contact-page.php
+++ contact-page.php
@@ -6,10 +6,10 @@ get_header(); ?>
 <section id="contact-page">
 	<div class="container woocommerce_container the_content">
 		<div class="row">
-			<div class="col-md-3">
+			<div class="col-md-3 order-2">
 				<?php dynamic_sidebar( 'contact_sidebar' ); ?>
 			</div>
-			<div class="col-md-9">
+			<div class="col-md-9 order-1">
 				<?php if ( have_posts() ) : ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php the_content(); ?>
