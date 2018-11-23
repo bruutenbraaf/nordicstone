96d9adbc559aaeead572fcef3e7093c6c5b0518c
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2018-11-23 10:37:49 +0100

    Page

--- page.php
+++ page.php
@@ -1,20 +1,15 @@
 <?php
 
 get_header(); ?>
-<section id="page-content">
 	<div class="container woocommerce_container the_content">
 		<div class="row">
-			<div class="col-md-8">
+			<div class="col-md-12">
 				<?php if ( have_posts() ) : ?>
 					<?php while ( have_posts() ) : the_post(); ?>
 						<?php the_content(); ?>
 					<?php endwhile; ?>
 				<?php endif; ?>
 			</div>
-			<div class="col-md-4">
-				Sidebar
-			</div>
 		</div>
 	</div>
-</section>
 <?php get_footer(); ?>
--- woocommerce.php
+++ woocommerce.php
@@ -3,9 +3,15 @@
 Template Name: WooCommerce
 */
 get_header(); ?>
-<section id="woocommerce_content">
-			<?php if (have_posts()) : while (have_posts()) : the_post();?>
-				<?php the_content(); ?>
-			<?php endwhile; endif; ?>			
-</section>
+	<div class="container woocommerce_container the_content">
+		<div class="row">
+			<div class="col-md-12">
+				<?php if ( have_posts() ) : ?>
+					<?php while ( have_posts() ) : the_post(); ?>
+						<?php the_content(); ?>
+					<?php endwhile; ?>
+				<?php endif; ?>
+			</div>
+		</div>
+	</div>
 <?php get_footer(); ?>
