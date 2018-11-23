4dd17998de7d272a86274e3d1f4cdc50813af914
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2018-11-23 10:12:21 +0100

    Woocommerce page

--- woocommerce.php
+++ woocommerce.php
@@ -3,11 +3,9 @@
 Template Name: WooCommerce
 */
 get_header(); ?>
-<div class="container woocommerce_container the_content">
-	<div class="row">
+<section id="woocommerce_content">
 			<?php if (have_posts()) : while (have_posts()) : the_post();?>
 				<?php the_content(); ?>
 			<?php endwhile; endif; ?>			
-	</div>
-</div>
+</section>
 <?php get_footer(); ?>
