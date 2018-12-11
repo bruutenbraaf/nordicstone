06526223b7701dea114181feef8ef5df9120b2a0
https://github.com/bruutenbraaf/bruutenbraf.git <info@bruutenbraaf.nl>
2018-12-11 20:02:32 +0100

    Sidebar

--- page.php
+++ page.php
@@ -34,7 +34,7 @@ get_header(); ?>
 	
 	<div class="container page_container page-content">
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-4 sidebar">
 				<?php dynamic_sidebar( 'page_sidebar' ); ?>
 			</div>
 			<div class="col-md-8">
